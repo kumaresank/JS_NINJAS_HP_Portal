f49648c3daee52bae61cd10b9a60479b23307a76
Rahul Badenkal <rahulbadenkal@gmail.com>
2024-11-07 17:20:13 +0530

    Added Slots

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { asyncHttpErrorWrapper, combineParamsAndBodyData, attachUser, checkRole } = require("./middleware");
-const { signupSchema, loginSchema, profileSchema, doctorsSchema } = require('./schema');
-const { signupController, loginController, profileController, fetchDoctorsController } = require('./controllers');
+const { signupSchema, loginSchema, profileSchema, doctorsSchema, addSlotSchema, getSlotsSchema} = require('./schema');
+const { signupController, loginController, profileController, fetchDoctorsController, addSlotController, getSlotsController } = require('./controllers');
 
 const router = express.Router();
 
 
 // Signup a new user
+// We are letting new doctors/admins also register by signing role in signup route. Not sure if this is the right thing to do`
 router.post('/auth/register', 
   asyncHttpErrorWrapper(combineParamsAndBodyData(signupSchema)), 
   asyncHttpErrorWrapper(signupController)
@@ -32,5 +33,20 @@ router.get('/doctors',
   asyncHttpErrorWrapper(fetchDoctorsController)
 );
 
+
+// List of doctors
+router.post('/slots/add',
+  asyncHttpErrorWrapper(attachUser),
+  asyncHttpErrorWrapper(checkRole(["admin"])),
+  asyncHttpErrorWrapper(combineParamsAndBodyData(addSlotSchema)),
+  asyncHttpErrorWrapper(addSlotController)
+);
+
+// List of doctors
+router.post('/slots/',
+  asyncHttpErrorWrapper(attachUser),
+  asyncHttpErrorWrapper(combineParamsAndBodyData(getSlotsSchema)),
+  asyncHttpErrorWrapper(getSlotsController)
+);
 
 module.exports = router;
